0197e59a6d2a56f6bcc6f34dd1aed37db6102577
Frederic Huang <kuofu.huang@prophetstor.com>
2020-08-01 02:01:51 +0800

    add gen doc library in Dockerfile

--- compile_proto_using_docker.sh
+++ compile_proto_using_docker.sh
@@ -1,43 +1,27 @@
 #!/bin/bash
 
-ALAMEA_GRPC_PYTHON_IMAGE_REPO="alameda/grpc_python"
-ALAMEA_GRPC_PYTHON_IMAGE_TAG="latest"
-ALAMEA_GRPC_PYTHON_IMAGE="$ALAMEA_GRPC_PYTHON_IMAGE_REPO:$ALAMEA_GRPC_PYTHON_IMAGE_TAG"
-ALAMEA_GRPC_PYTHON_IMAGE_DOCKERFILE=Dockerfile_gRPC_python
-REQUIREMENT_MD5=`md5sum requirements.txt | awk '{print $1}'`
-
 ALAMEA_GRPC_GO_IMAGE_REPO="alameda/grpc_go"
 ALAMEA_GRPC_GO_IMAGE_TAG="latest"
 ALAMEA_GRPC_GO_IMAGE="$ALAMEA_GRPC_GO_IMAGE_REPO:$ALAMEA_GRPC_GO_IMAGE_TAG"
 ALAMEA_GRPC_GO_IMAGE_DOCKERFILE=Dockerfile_gRPC_go
 
 generate_dockerfiles(){
-    cat > $ALAMEA_GRPC_PYTHON_IMAGE_DOCKERFILE - <<EOF
-FROM python:slim
-ARG REQUIREMENT_MD5
-ARG DOCKERFILE_MD5
-ENV REQUIREMENT_MD5=\$REQUIREMENT_MD5 DOCKERFILE_MD5=\$DOCKERFILE_MD5
-COPY requirements.txt .
-RUN pip install -r requirements.txt
-EOF
-
     cat > $ALAMEA_GRPC_GO_IMAGE_DOCKERFILE - <<EOF
 FROM golang:stretch
 ARG DOCKERFILE_MD5
-ENV DOCKERFILE_MD5=\$DOCKERFILE_MD5 PROTOC_VER=3.11.3 OS_ARC=linux-x86_64 PROTOC_GEN_GO_VER=v1.3.3
-RUN apt-get update && apt-get install unzip -y && \\
+ENV DOCKERFILE_MD5=\$DOCKERFILE_MD5 PROTOC_VER=3.11.3 OS_ARC=linux-x86_64 PROTOC_GEN_GO_VER=v1.3.3 PROTOC_GEN_DOC_VER=v1.3.2
+COPY setup.py .
+RUN apt-get update && apt-get install unzip python3 python3-pip -y && \\
 curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v\$PROTOC_VER/protoc-\$PROTOC_VER-\$OS_ARC.zip && \\
 unzip protoc-\$PROTOC_VER-\$OS_ARC.zip -d /usr/local && rm protoc-\$PROTOC_VER-\$OS_ARC.zip && \\
 go get -d -u github.com/golang/protobuf/protoc-gen-go && \\
 git -C "\$(go env GOPATH)"/src/github.com/golang/protobuf checkout \$PROTOC_GEN_GO_VER && \\
 go install github.com/golang/protobuf/protoc-gen-go && \\
+GO111MODULE=on go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@\$PROTOC_GEN_DOC_VER && \\
+perl -lne 'print if /INSTALL_REQUIRES =/ .. /\)/' setup.py | grep -v \( | grep -v \) |  awk -F  "'" '{print \$2}' > requirements.txt && \\
+pip3 install -r requirements.txt && rm requirements.txt && rm setup.py && \\
 rm -rf /var/lib/apt/lists/*
 EOF
-}
-
-build_python_image(){
-    local DOCKERFILE_MD5=`md5sum $ALAMEA_GRPC_PYTHON_IMAGE_DOCKERFILE | awk '{print $1}'`
-    docker build --build-arg REQUIREMENT_MD5=$REQUIREMENT_MD5 --build-arg DOCKERFILE_MD5=$DOCKERFILE_MD5 . -t $ALAMEA_GRPC_PYTHON_IMAGE -f $ALAMEA_GRPC_PYTHON_IMAGE_DOCKERFILE 
 }
 
 build_go_image(){
@@ -45,27 +29,7 @@ build_go_image(){
     docker build --build-arg DOCKERFILE_MD5=$DOCKERFILE_MD5 . -t $ALAMEA_GRPC_GO_IMAGE -f $ALAMEA_GRPC_GO_IMAGE_DOCKERFILE 
 }
 
-compile_grpc_python(){
-    echo "Check gRPC python image."
-    local DOCKERFILE_MD5=`md5sum $ALAMEA_GRPC_PYTHON_IMAGE_DOCKERFILE | awk '{print $1}'`
-    if ! docker images $ALAMEA_GRPC_PYTHON_IMAGE | grep $ALAMEA_GRPC_PYTHON_IMAGE_REPO > /dev/null 2>&1; then
-        echo "Build new image $ALAMEA_GRPC_PYTHON_IMAGE";
-        build_python_image
-    fi
-    if ! docker run --rm $ALAMEA_GRPC_PYTHON_IMAGE sh -c "
-        if [ \"$REQUIREMENT_MD5\" != \"\$REQUIREMENT_MD5\" ] || [ \"$DOCKERFILE_MD5\" != \"\$DOCKERFILE_MD5\" ]; then
-            exit 1;
-        fi"; then
-        echo "Refresh image $ALAMEA_GRPC_PYTHON_IMAGE";
-        docker rmi $ALAMEA_GRPC_PYTHON_IMAGE;
-        build_python_image
-    fi
-    echo "Start compiling proto files to python files."
-    docker run --rm -v $(pwd):$(pwd) -w $(pwd) $ALAMEA_GRPC_PYTHON_IMAGE bash -c "for pt in \$(find . | grep \\\.proto\$ | grep -v ^\\\./include | grep -v ^\\\./google);do python -m grpc_tools.protoc -I . -I include/ --python_out=./ --grpc_python_out=./ \$pt; done"
-    echo "Finish compiling proto files to python files."
-}
-
-compile_grpc_go(){
+compile_grpc(){
     echo "Check gRPC go image."
     local DOCKERFILE_MD5=`md5sum $ALAMEA_GRPC_GO_IMAGE_DOCKERFILE | awk '{print $1}'`
     if ! docker images $ALAMEA_GRPC_GO_IMAGE | grep $ALAMEA_GRPC_GO_IMAGE_REPO > /dev/null 2>&1; then
@@ -80,17 +44,16 @@ compile_grpc_go(){
         docker rmi $ALAMEA_GRPC_GO_IMAGE;
         build_go_image
     fi
-    echo "Start compiling proto files to go files."
-    docker run --rm -v $(pwd):$(pwd) -w $(pwd) $ALAMEA_GRPC_GO_IMAGE bash -c "for pt in \$(find . | grep \\\.proto\$ | grep -v ^\\\./include | grep -v ^\\\./google);do protoc -I . -I include/ \$pt --go_out=paths=source_relative,plugins=grpc:.; done"
-    echo "Finish compiling proto files to go files."
+    echo "Start compiling proto files."
+    docker run --rm -v $(pwd):$(pwd) -w $(pwd) $ALAMEA_GRPC_GO_IMAGE bash -c "for pt in \$(find . | grep \\\.proto\$ | grep -v ^\\\./include | grep -v ^\\\./google);do protoc -I . -I include/ \$pt --go_out=paths=source_relative,plugins=grpc:.; python3 -m grpc_tools.protoc -I . -I include/ --python_out=./ --grpc_python_out=./ \$pt; done"
+    docker run --rm -v $(pwd):$(pwd) -w $(pwd) $ALAMEA_GRPC_GO_IMAGE bash -c "protoc -I . -I include/ --doc_out=./ --doc_opt=html,index.html $(find . | grep \\\.proto\$ | grep -v ^\\\./include | grep -v ^\\\./google | tr '\n' ' ');"
+    echo "Finish compiling proto files."
 }
 
 remove_dockerfiles(){
-   [ -f $ALAMEA_GRPC_PYTHON_IMAGE_DOCKERFILE ] && rm $ALAMEA_GRPC_PYTHON_IMAGE_DOCKERFILE
    [ -f $ALAMEA_GRPC_GO_IMAGE_DOCKERFILE ] && rm $ALAMEA_GRPC_GO_IMAGE_DOCKERFILE
 }
 
 generate_dockerfiles
-compile_grpc_python
-compile_grpc_go
+compile_grpc
 remove_dockerfiles
